6305e2b69dc1e034639d4a5c677aea74ca13713b
ionutfrent15 <ionutfrent15@gmail.com>
2019-11-05 12:33:49 +0200

    menu and login

--- inter-map-client/src/app/auth/register/validators.ts
+++ inter-map-client/src/app/auth/register/validators.ts
@@ -10,12 +10,12 @@ export const PasswordValidation = [
 
 export class RepeatPasswordEStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    return (control && control.parent.get('password').value !== control.parent.get('passwordAgain').value && control.dirty)
+    return (control && control.parent.get('password').value !== control.parent.get('passwordAgain').value && control.dirty);
   }
 }
 export function RepeatPasswordValidator(group: FormGroup) {
   const password = group.controls.password.value;
   const passwordConfirmation = group.controls.passwordAgain.value;
 
-  return password === passwordConfirmation ? null : { passwordsNotEqual: true }     
+  return password === passwordConfirmation ? null : { passwordsNotEqual: true }
 }
