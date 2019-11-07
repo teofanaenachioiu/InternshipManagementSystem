529a03a15144b1a1bebdc17919d9634ea6a7b4e3
Ami13F <ameliaforgacs@gmail.com>
2019-11-07 10:46:05 +0200

    [refactor] Replace employer with company
    - replace repository name
    - add gitignore configuration for vscode

--- intern-server/src/main/java/com/intern/Internship/repository/EmployerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.intern.Internship.repository;
-
-import com.intern.Internship.model.Employer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployerRepository extends JpaRepository<Employer,String> {
-}
