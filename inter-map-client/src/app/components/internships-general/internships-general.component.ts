a34e8df7c198128782a6be4c1698d048545e194f
teofanaenachioiu <teofanaenachioiu@yahoo.com>
2019-11-09 18:10:20 +0200

    added homepage, we need to refactor css files

---
+++ inter-map-client/src/app/components/internships-general/internships-general.component.ts
@@ -0,0 +1,98 @@
+import {Component, HostListener, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-internships-general',
+  templateUrl: './internships-general.component.html',
+  styleUrls: ['./internships-general.component.css']
+})
+export class InternshipsGeneralComponent implements OnInit {
+
+  constructor() { }
+
+  CAROUSEL_BREAKPOINT = 768;
+  carouselDisplayMode = 'multiple';
+
+  cards = [
+    {
+      title: 'Card Title 1',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 2',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 3',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 4',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 5',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 6',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 7',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 8',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 9',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+  ];
+  slides: any = [[]];
+  chunk(arr, chunkSize) {
+    const R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
+  }
+  ngOnInit() {
+    this.slides = this.chunk(this.cards, 3);
+
+    if (window.innerWidth <= this.CAROUSEL_BREAKPOINT) {
+      this.carouselDisplayMode = 'single';
+    } else {
+      this.carouselDisplayMode = 'multiple';
+    }
+  }
+
+  @HostListener('window:resize')
+  onWindowResize() {
+    if (window.innerWidth <= this.CAROUSEL_BREAKPOINT) {
+      this.carouselDisplayMode = 'single';
+    } else {
+      this.carouselDisplayMode = 'multiple';
+    }
+  }
+
+}
