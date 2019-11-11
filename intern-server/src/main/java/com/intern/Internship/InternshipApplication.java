66fef1069583af9bf6cd6d3e44fc5e93845d9f1b
Ami13F <ameliaforgacs@gmail.com>
2019-11-11 15:19:11 +0200

    [refactor] Remove unecessary configurations

--- intern-server/src/main/java/com/intern/Internship/InternshipApplication.java
+++ intern-server/src/main/java/com/intern/Internship/InternshipApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan // maybe delete this
 public class InternshipApplication extends SpringBootServletInitializer {
 
 	@Override
