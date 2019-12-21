f19e1687dc6f2f4863035a5e374c97d33f98c1ed
Tudor-Adrian Gînga <tudor.adrian.ginga@gmail.com>
2019-12-21 19:14:48 +0200

    [test] Tested CompanyRepository

--- intern-server/src/main/java/com/intern/Internship/repository/CompanyRepository.java
+++ intern-server/src/main/java/com/intern/Internship/repository/CompanyRepository.java
@@ -2,6 +2,8 @@ package com.intern.Internship.repository;
 
 import com.intern.Internship.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, String> {
 }
