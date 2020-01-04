f61bef359cb03e0d50fbaf2695c8376222e5ad64
simo213 <enasoae.simona@yahoo.com>
2020-01-04 13:41:37 +0200

    get all applications for a candidate

--- intern-server/src/main/java/com/intern/Internship/controller/ApplicationController.java
+++ intern-server/src/main/java/com/intern/Internship/controller/ApplicationController.java
@@ -1,8 +1,8 @@
 package com.intern.Internship.controller;
 
 import com.intern.Internship.model.Application;
-import com.intern.Internship.model.Candidate;
 import com.intern.Internship.model.dto.ApplicationDTO;
+import com.intern.Internship.model.dto.ApplicationRequest;
 import com.intern.Internship.model.enums.ApplicationStatus;
 import com.intern.Internship.service.ApplicationService;
 import com.intern.Internship.service.CandidateService;
@@ -10,6 +10,9 @@ import com.intern.Internship.service.InternshipService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/application")
@@ -25,7 +28,7 @@ public class ApplicationController {
     private CandidateService candidateService;
 
     @PostMapping()
-    public ResponseEntity<ApplicationDTO> save(@RequestBody ApplicationDTO applicationDTO) {
+    public ResponseEntity<ApplicationDTO> save(@RequestBody ApplicationRequest applicationDTO) {
         try {
             Application application =new Application(ApplicationStatus.Applied,
                     applicationDTO.getExtraMessage(),
@@ -34,6 +37,7 @@ public class ApplicationController {
 
             Application result = applicationService.save(application);
             ApplicationDTO resultDTO= new ApplicationDTO(
+                    result.getID(),
                     result.getCandidate().getID(),
                     result.getInternship().getID(),
                     result.getExtraMessage());
@@ -41,6 +45,17 @@ public class ApplicationController {
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().body(new ApplicationDTO());
+        }
+    }
+    @GetMapping
+    public ResponseEntity<List<ApplicationDTO>> findApplicationByCandidate(@RequestParam("username") String username) {
+
+        try {
+            List<ApplicationDTO> internships = applicationService.getApplicationsByUsername(username);
+            return ResponseEntity.ok().body(internships);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(new ArrayList<ApplicationDTO>());
         }
     }
 }
--- intern-server/src/main/java/com/intern/Internship/model/dto/ApplicationDTO.java
+++ intern-server/src/main/java/com/intern/Internship/model/dto/ApplicationDTO.java
@@ -1,22 +1,66 @@
 package com.intern.Internship.model.dto;
 
+import com.intern.Internship.model.Feedback;
+import com.intern.Internship.model.enums.ApplicationStatus;
+import com.intern.Internship.model.enums.InternshipStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
 public class ApplicationDTO {
+    private String id;
     private String idCandidate;
     private String idInternship;
+    private String idCompany;
+    private String nameCompany;
     private String extraMessage;
+    private String name;
+    private LocalDate startTime;
+    private LocalDate endTime;
+    private Boolean paid;
+    private int nrMonths;
+    private String description;
+    private int nrApplicants;
+    private InternshipStatus status;
+    private String location;
+    private LocalDate addedDate;
+    private ApplicationStatus applicationStatus;
 
-    public ApplicationDTO(String idCandidate, String idInternship, String extraMessage) {
+    private Set<FeedbackDTO> feedbacks;
+
+    public ApplicationDTO(String id,String idCandidate, String idInternship, String extraMessage) {
+        this.id=id;
         this.idCandidate = idCandidate;
         this.idInternship = idInternship;
         this.extraMessage = extraMessage;
+    }
+
+    public ApplicationDTO(Map<String,?> map) {
+        this.id = (String) map.get("id");
+        this.idCandidate = (String) map.get("candidateId");
+        this.idInternship = (String) map.get("internshipId");
+        this.idCompany = (String) map.get("companyId");
+        this.nameCompany = (String) map.get("companyName");
+        this.name = (String) map.get("name");
+        this.startTime = (LocalDate) map.get("startTime");
+        this.endTime = (LocalDate) map.get("endTime");
+        this.paid = (Boolean) map.get("paid");
+        this.nrMonths = (Integer) map.get("nrMonths");
+        this.description = (String) map.get("description");
+        this.nrApplicants = (Integer) map.get("nrApplicants");
+        this.status = (InternshipStatus) map.get("status");
+        this.location = (String) map.get("location");
+        this.addedDate = (LocalDate) map.get("addedDate");
+        this.extraMessage = (String) map.get("extraMessage");
+        this.applicationStatus = (ApplicationStatus) map.get("applicationStatus");
     }
 }
