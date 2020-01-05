f155262829ca335a09f29563053da363b5d43352
Sebastian Giurgiu <sebastiangiurgiu1998@gmail.com>
2020-01-05 18:44:25 +0200

    adaugare aplicare internship

--- inter-map-client/src/app/internship-app/create-application/create-application.component.ts
+++ inter-map-client/src/app/internship-app/create-application/create-application.component.ts
@@ -3,7 +3,7 @@ import { InternshipService } from '../internship.service';
 import { Application } from '../data/Application';
 import { catchError } from 'rxjs/operators';
 import { AuthService } from 'src/app/auth/auth.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-application',
@@ -13,7 +13,8 @@ import { ActivatedRoute } from '@angular/router';
 export class CreateApplicationComponent implements OnInit {
   coverLetter : string;
   id : string;
-  constructor(private internshipService: InternshipService,private authService : AuthService,private route: ActivatedRoute) { }
+  constructor(private internshipService: InternshipService,private authService : AuthService,
+    private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
@@ -25,6 +26,7 @@ export class CreateApplicationComponent implements OnInit {
     console.log(this.id);
     let application = new Application(this.authService.currentUserValue.username,this.id,this.coverLetter);
     this.internshipService.addApplication(application);
+    this.router.navigate(['internship-app/applications']);
   }
 
 }
--- inter-map-client/src/app/internship-app/data/Internship.ts
+++ inter-map-client/src/app/internship-app/data/Internship.ts
@@ -1,22 +1,17 @@
 
+
 export class Internship {
-    constructor(                                                
-      public name: string,
-      public startTime: Date,
-      public endTime: Date,
-      public paid: boolean ,
-      public nrMonths: number,
-      public description: string,
-      public nrApplicant: number,
-      public status: string,
-      public location: string,
-      public addedDate: Date,
-      public company: string,
-      public imagePath: string,
-      public interests: string,
-      public numberOfFeedbacks: number,
-      public averageOfFeedbacks: number,
-      public rating: number
-      ) { }
-  }
+  constructor(
+    public name: string,
+    public paid: boolean ,
+    public nrMonths: number,
+    public description: string,
+    public location: string,
+    public company: string,
+    public imagePath: string,
+    public rating: number,
+    public interests: string,
+    public status: string
+    ) { }
+}
 
