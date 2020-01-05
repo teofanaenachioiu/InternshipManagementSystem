b718dfc70415520365f6be51c1cac816bd6dd5fa
Sebastian Giurgiu <sebastiangiurgiu1998@gmail.com>
2020-01-05 18:22:26 +0200

    Rezolvare conflicte de pe master

--- inter-map-client/src/app/internship-app/internship-app.module.ts
+++ inter-map-client/src/app/internship-app/internship-app.module.ts
@@ -15,30 +15,7 @@ import {RatingModule} from 'ng-starrating';
 import {FilterRatingPipe} from './data/filter-rating.pipe';
 import {OrderModule} from 'ngx-order-pipe';
 
-import {FilterForMultipleInterestPipe} from './data/filter-for-multiple-interest.pipe';
-import {CandidateProfileComponent} from './profiles/candidate-profile/candidate-profile.component';
-import {CandidateMenuComponent} from '../components/candidate-menu/candidate-menu.component';
-import {CompanyMenuComponent} from '../components/company-menu/company-menu.component';
-import {PersonalDetailsComponent} from './profiles/candidate-profile/personal-details/personal-details.component';
-import {ContactComponent} from './profiles/candidate-profile/contact/contact.component';
-import {StudiesComponent} from './profiles/candidate-profile/studies/studies.component';
-import {WorkExperienceComponent} from './profiles/candidate-profile/work-experience/work-experience.component';
-import {LanguagesComponent} from './profiles/candidate-profile/languages/languages.component';
-import {CompanyProfileComponent} from './profiles/company-profile/company-profile.component';
-import {AboutComponent} from './profiles/company-profile/about/about.component';
-import {ProfileComponent} from './profiles/company-profile/profile/profile.component';
-import {TellUsMoreComponent} from './profiles/candidate-profile/tell-us-more/tell-us-more.component';
-import {InterestsComponent} from './profiles/interests/interests.component';
-import {CompanyInternshipsComponent} from './profiles/company-profile/company-internships/company-internships.component';
-import {AddModalComponent} from './profiles/company-profile/add-modal/add-modal.component';
-import {ConfirmRemoveInternshipComponent} from './profiles/company-profile/company-internships/confirm-remove-internship/confirm-remove-internship.component';
-import {AddFormComponent} from './profiles/company-profile/add-modal/add-form/add-form.component';
-import {EditModalComponent} from './profiles/company-profile/edit-modal/edit-modal.component';
-import {EditFormComponent} from './profiles/company-profile/edit-modal/edit-form/edit-form.component';
-import {MaterialFileInputModule} from 'ngx-material-file-input';
-import {PersonalDetailsViewComponent} from './profiles/candidate-profile/personal-details-view/personal-details-view.component';
-import {CandidateApplicationsListComponent} from './candidate-applications-list/candidate-applications-list.component';
-import {CandidateApplicationsListItemComponent} from './candidate-applications-list/candidate-applications-list-item/candidate-applications-list-item.component';
+
 import {CustomSpinnerComponent} from '../components/custom-spinner/custom-spinner.component';
 import { ContactViewComponent } from './profiles/candidate-profile/contact-view/contact-view.component';
 import { InterestsViewComponent } from './profiles/interests-view/interests-view.component';
@@ -46,23 +23,9 @@ import { TellUsMoreViewComponent } from './profiles/candidate-profile/tell-us-mo
 import { LanguagesViewComponent } from './profiles/candidate-profile/languages-view/languages-view.component';
 import { WorkExperienceViewComponent } from './profiles/candidate-profile/work-experience-view/work-experience-view.component';
 import { StudiesViewComponent } from './profiles/candidate-profile/studies-view/studies-view.component';
-=======
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
-    MatMenuModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatRadioModule,
-    MatChipsModule,
-    MatTooltipModule,
-    MatDialogModule, MatCardModule,
-	  MatFormFieldModule,
+import {
+  MatCardModule,
+	MatFormFieldModule,
   MatInputModule,
   MatSelectModule,
   MatButtonModule,
@@ -130,7 +93,7 @@ const materialComponents = [
   MatTooltipModule,
 
   MatProgressSpinnerModule,
-  MatAutocompleteModule
+  MatAutocompleteModule,
   MatDialogModule
 ];
 
@@ -171,7 +134,7 @@ const materialComponents = [
     TellUsMoreViewComponent,
     LanguagesViewComponent,
     WorkExperienceViewComponent,
-    StudiesViewComponent
+    StudiesViewComponent,
     ProfileViewComponent,
     InternshipModalDetailsComponent,
     CreateApplicationComponent,
